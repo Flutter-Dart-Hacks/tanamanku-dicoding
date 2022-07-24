a434c315912214b82a8b6e94f169baeb08bc0dbe
GulajavaMinistudio <gulajava.mini@gmail.com>
2022-07-25 05:35:35 +0700

    Penambahan aset svg dan keterangan tanaman

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,3 +4,7 @@ const primaryColorGreen = Color(0xff1d554a);
 const backgroundColorGreen = Color(0xffccdfdb);
 const inputFieldBg = Color(0xffd9e4db);
 const iconColorLoginGreen = Color(0xff557357);
+
+const stateCuacaBerawan = 'Berawan';
+const stateCuacaPanas = 'Panas';
+const stateCuacaHujan = 'Hujan';
--- lib/models/plant_item.dart
+++ lib/models/plant_item.dart
@@ -1,14 +1,12 @@
 class PlantItem {
   String nama;
   String keterangan;
-  String jenisSiram;
   String periodeSiram;
   String cuaca;
 
   PlantItem({
     required this.nama,
     required this.keterangan,
-    required this.jenisSiram,
     required this.periodeSiram,
     required this.cuaca,
   });
@@ -17,30 +15,30 @@ class PlantItem {
 List<PlantItem> listTanaman = [
   PlantItem(
     nama: 'Succulent',
-    keterangan: '',
-    jenisSiram: 'Air dengan jumlah sedang',
+    keterangan:
+        'Tumbuhan dengan organ yang menebal, berdaging, dan membengkak, biasanya untuk menyimpan air di iklim atau kondisi tanah kering.',
     periodeSiram: '3 hari sekali',
-    cuaca: '',
+    cuaca: 'Berawan',
   ),
   PlantItem(
     nama: 'Haworthia',
-    keterangan: '',
-    jenisSiram: 'Air dengan jumlah sedang',
-    periodeSiram: '3 hari sekali',
-    cuaca: '',
+    keterangan:
+        'Tanaman sukulen asal Afrika Selatan. Ia menyukai matahari, tidak perlu sering disiram, sehingga cocok untuk mereka yang tak punya banyak waktu mengurus tanaman.',
+    periodeSiram: '4 hari sekali',
+    cuaca: 'Panas',
   ),
   PlantItem(
     nama: 'Kaktus',
-    keterangan: '',
-    jenisSiram: 'Air dengan jumlah sedang',
-    periodeSiram: '3 hari sekali',
-    cuaca: '',
+    keterangan:
+        'Tanaman yang biasa tumbuh di daerah gurun yang panas dengan daun yang telah termodifikasi menjadi duri kaktus dapat hidup di daerah yang kering.',
+    periodeSiram: '5 hari sekali',
+    cuaca: 'Panas',
   ),
   PlantItem(
     nama: 'Aglonema',
-    keterangan: '',
-    jenisSiram: 'Air dengan jumlah sedang',
-    periodeSiram: '3 hari sekali',
-    cuaca: '',
+    keterangan:
+        'Tanaman hias dari jenis talas-talasan yang berada di hutan hujan tropis. Tanaman ini menyukai air dan kondisi lingkungan yang lembab.',
+    periodeSiram: '2 hari sekali',
+    cuaca: 'Hujan',
   ),
 ];
